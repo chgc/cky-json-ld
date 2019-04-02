373d243abbbe7673174f32dbdf9f8c6bb55e8477
Kevin Yang <chgc_kevin@hotmail.com>
2019-04-02 12:23:04 +0800

    cky-json-ld first init

---
+++ projects/cky-json-ld/README.md
@@ -0,0 +1,24 @@
+# CkyJsonLd
+
+This library was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.0.0-beta.9.
+
+## Code scaffolding
+
+Run `ng generate component component-name --project cky-json-ld` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module --project cky-json-ld`.
+> Note: Don't forget to add `--project cky-json-ld` or else it will be added to the default project in your `angular.json` file. 
+
+## Build
+
+Run `ng build cky-json-ld` to build the project. The build artifacts will be stored in the `dist/` directory.
+
+## Publishing
+
+After building your library with `ng build cky-json-ld`, go to the dist folder `cd dist/cky-json-ld` and run `npm publish`.
+
+## Running unit tests
+
+Run `ng test cky-json-ld` to execute the unit tests via [Karma](https://karma-runner.github.io).
+
+## Further help
+
+To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
---
+++ projects/cky-json-ld/src/lib/cky-json-ld.component.ts
@@ -0,0 +1,44 @@
+import { Component, Inject, Optional } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { filter, map } from 'rxjs/operators';
+import { JSONLD } from './token';
+
+@Component({
+  selector: 'cky-json-ld',
+  template: ``
+})
+export class CkyJsonLdComponent {
+  constructor(
+    route: ActivatedRoute,
+    router: Router,
+    @Optional() @Inject(JSONLD) ldName: string
+  ) {
+    router.events
+      .pipe(
+        filter(e => e instanceof NavigationEnd),
+        map(e => route.root.firstChild.snapshot.data)
+      )
+      .subscribe(data => {
+        this.setJSON_LD(data[ldName || 'ld'] || '');
+      });
+  }
+
+  setJSON_LD(value) {
+    const schemaContent = value
+      ? JSON.stringify(value, null, 2).replace(/<\/script>/g, '<\\/script>')
+      : '';
+    let script: HTMLScriptElement = document.querySelector(
+      'script[type="application/ld+json"]'
+    );
+    if (script == null) {
+      script = document.createElement('script');
+      script.type = 'application/ld+json';
+      document.body.appendChild(script);
+    }
+    script.innerHTML = schemaContent;
+
+    if (schemaContent.length === 0) {
+      document.body.removeChild(script);
+    }
+  }
+}
